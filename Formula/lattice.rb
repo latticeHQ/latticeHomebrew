c37a77209bd0f66474c19cae083ad0819d166691
Lattice CI <ci@latticeruntime.com>
2026-02-09 22:16:33 +0530

    lattice 0.11.3

--- Formula/lattice.rb
+++ Formula/lattice.rb
@@ -4,27 +4,27 @@
 class Lattice < Formula
   desc "A self-hosted remote development platform"
   homepage "https://latticeruntime.com"
-  version "0.8.1"
+  version "0.11.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_darwin_arm64.zip"
-      sha256 "b3676a236f520f2c9f730020850af2c3343ad88954e9dbb57016ae4427128c87"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.11.3/lattice_0.11.3_darwin_arm64.zip"
+      sha256 "9daa3afac640556bc6659a392ba23fcd98386b1e5cdd13adb9a70e6f48ed1eb2"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_darwin_amd64.zip"
-      sha256 "377f299d4d916278831a58a92616dc7fa77442983d5661ef03742aebc84fd1f3"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.11.3/lattice_0.11.3_darwin_amd64.zip"
+      sha256 "a63cce978856661fd62f6619ccc11ff70484945797f4f5690f432f0f8f7f1044"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_linux_arm64.tar.gz"
-      sha256 "bce522dc5814a4e3bc596adba4944b416cbe5a69adaf7ae93f6eab4eee195d3d"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.11.3/lattice_0.11.3_linux_arm64.tar.gz"
+      sha256 "398f055bf781a3a440cea96d0aad35de23d5fdabe793a23487ebad2e228443ed"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/latticeHQ/lattice/releases/download/v0.8.1/lattice_0.8.1_linux_amd64.tar.gz"
-      sha256 "4bb9322582e43046e73ef98944f7044550bdcd4b886e9ae89f393f1db9a10750"
+      url "https://github.com/latticeHQ/lattice/releases/download/v0.11.3/lattice_0.11.3_linux_amd64.tar.gz"
+      sha256 "401429dd4b3a33b150b7638f32c0d97ac915798bcef443e5c282dc9f252cef78"
     end
   end
 
